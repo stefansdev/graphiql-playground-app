cadc752a291af99020f678bdfae40d64542da48e
Stefan Stankovic <s@stefans.dev>
2023-04-14 07:45:06 +0200

    styling

--- components/GraphiqlWrapper.js
+++ components/GraphiqlWrapper.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import GraphiQL from 'graphiql';
 import Contribute from '@/components/Contribute';
 import 'graphiql/graphiql.min.css';
@@ -8,31 +8,35 @@ import 'graphiql/graphiql.min.css';
 const GraphiQLWrapper = () => {
 	const [url, setUrl] = useState('');
 	const [token, setToken] = useState(null);
+
 	return (
 		<>
-			<div className="flex bg-blue-200 py-2 px-4 space-x-4 text-sm font-mono">
-				<div>
-					<input
-						type="text"
-						id="url"
-						placeholder="GraphQL URL"
-						value={url}
-						onChange={(e) => setUrl(e.target.value)}
-						className="rounded-md border min-w-[300px] bg-white outline-none px-4 py-2 "
-					/>
-				</div>
-				<div>
-					<input
-						type="text"
-						id="token"
-						placeholder="Bearer token"
-						value={token}
-						onChange={(e) => setToken(e.target.value)}
-						className="rounded-md border min-w-[300px] bg-white outline-none px-4 py-2 "
-					/>
+			<div className="flex bg-blue-200 py-2 px-4 space-x-4 text-sm font-mono justify-between items-center">
+				<div className="flex space-x-4">
+					<div>
+						<input
+							type="text"
+							id="url"
+							placeholder="GraphQL URL"
+							value={url}
+							onChange={(e) => setUrl(e.target.value)}
+							className="rounded-md border min-w-[300px] bg-white outline-none px-4 py-2"
+						/>
+					</div>
+					<div>
+						<input
+							type="text"
+							id="token"
+							placeholder="Bearer token"
+							value={token}
+							onChange={(e) => setToken(e.target.value)}
+							className="rounded-md border min-w-[300px] bg-white outline-none px-4 py-2"
+						/>
+					</div>
 				</div>
 				<Contribute />
 			</div>
+
 			<GraphiQL
 				fetcher={async (graphQLParams) => {
 					const data = await fetch(url, {
